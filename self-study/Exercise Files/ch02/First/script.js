54e782e1eeae1e09308b87ca3181231b45eb9bfb
mehmetsaidcoskun <39768263+mehmetsaidcoskun@users.noreply.github.com>
2018-07-04 14:35:54 +0200

    some notes

--- self-study/Exercise Files/ch02/First/script.js
+++ self-study/Exercise Files/ch02/First/script.js
@@ -1,1 +1,2 @@
-alert("Hello World");
+var name = prompt("What?")
+alert("Hello, " + name);
---
+++ self-study/lynda.js
@@ -0,0 +1,66 @@
+/*
+var grade = "Diesel";
+
+switch ( grade ) {
+	case "Regular":
+		console.log("$3.15");
+		break;
+
+	case "Premium":
+		console.log("$3.47");
+		break;
+
+	case "Diesel":
+		console.log("$3.68");
+		break;
+	
+	default:
+		console.log("not valid");
+}
+*/
+
+/*
+var c;
+c = "I am a";
+c = c + " String!";
+console.log(c);
+*/
+
+/*
+function myFunction () {
+	console.log("This is a function.");
+}
+
+var d = 123;
+switch ( d ) {
+	case 123:
+	myFunction();
+		break;
+
+	default:
+	console.log("not");
+}
+*/
+
+/*
+function plusFunction (a,b) {
+	console.log(a+b);
+}
+
+plusFunction(1,1);
+plusFunction(100,2334);
+*/
+
+/*
+var x;
+function simpleFunction() {
+	x = 500;
+console.log(x);
+}
+simpleFunction();
+console.log(x);
+*/
+
+var myName = "Mehmet Said";
+console.log(myName.length);
+
